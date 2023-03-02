b79e3c55bcb11a7569ccb417b2f69525fcb1d915
fast-typing <82109250+fast-typing@users.noreply.github.com>
2023-03-02 19:15:09 +0400

    Delete src directory

--- src/components/Button/AddCartButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OrderButton(props) {
-    return (
-        <button onClick={props.handleClick} className='order-button'>
-            Добавить в корзину
-        </button>
-    )
-}
